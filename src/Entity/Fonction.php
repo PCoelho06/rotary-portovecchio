1fb92d394f869138a06668b3b0de04e98923d76c
PCoelho06 <p.coelho@lapinou.tech>
2024-03-21 16:40:05 +0100

    Added Fonction entity

---
+++ src/Entity/Fonction.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FonctionRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: FonctionRepository::class)]
+class Fonction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank()]
+    #[Assert\Regex("/^[[:alpha:]]+$/")]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ tests/Entity/FonctionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Fonction;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class FonctionTest extends KernelTestCase
+{
+    public function getEntity(): Fonction
+    {
+        return (new Fonction())->setName('Président');
+    }
+
+    public function assertHasErrors(Fonction $fonction, int $nbErrors = 0)
+    {
+        self::bootKernel();
+        $error = self::getContainer()->get('validator')->validate($fonction);
+
+        $this->assertCount($nbErrors, $error);
+    }
+
+    public function testValidFonction()
+    {
+        $this->assertHasErrors($this->getEntity(), 0);
+    }
+
+    public function testInvalidFonction()
+    {
+        $this->assertHasErrors($this->getEntity()->setName('123'), 1);
+        $this->assertHasErrors($this->getEntity()->setName('Président1'), 1);
+    }
+
+    public function testInvalidBlankFonction()
+    {
+        $this->assertHasErrors($this->getEntity()->setName(''), 1);
+    }
+}
